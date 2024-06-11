1807138708495bff768f5d5341a66e3076e8a9a4
lucatoneybuzz <lucatoney23@gmail.com>
2024-06-12 00:57:25 +0100

    inside-2

---
+++ BACKEND/Message/routes/contactRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { sendEmail } = require('../controllers/contactController');
+
+router.post('/contact', sendEmail);
+
+module.exports = router;
---
+++ BACKEND/Message/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const contactRoutes = require('./routes/contactRoutes');
+const cors = require('cors');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Use the contactRoutes
+app.use('/', contactRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
